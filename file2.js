b9977c817ea9547c89b6b40f17648132f4a4e307
nghiemduong2000 <nghiemduong2000@gmail.com>
2021-04-23 10:43:53 +0700

    Do you hear me ??

--- file2.js
+++ file2.js
@@ -1,4 +1,5 @@
 const file1 = () => {
+<<<<<<< HEAD
   console.log("Đi vào trong!");
 };
 
@@ -12,4 +13,11 @@ const file2_2 = () => {
 
 const masterHackerProfessional = (a, b) => {
   return a + b;
+=======
+  console.log("Do you hear me? Hah??");
+};
+
+const masterHackerProfessional = () => {
+  console.log("Hello World", 1 + 1 + 1 + 2);
+>>>>>>> Do you hear me ??
 };
