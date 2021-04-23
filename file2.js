0fce83ba8b483be96d11fab5f0fbb27184999e2a
sonhip <sonhipkmhd@gmail.com>
2021-04-23 10:13:00 +0700

    add feature 2_2

--- file2.js
+++ file2.js
@@ -1,3 +1,7 @@
 const file2 = () => {
-  console.log("This's file 2");
+  console.log("this is the fucking word?");
+  console.log("this is my life!");
+};
+const file2_2 = () => {
+  console.log("this is feature for fun");
 };
